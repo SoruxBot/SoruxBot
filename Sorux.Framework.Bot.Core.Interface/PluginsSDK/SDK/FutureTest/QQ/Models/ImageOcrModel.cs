547729a6145d61dd54de818251c810ed5cb75208
Mo Epic <epicmo>
2023-01-08 22:08:00 +0800

    feat:qq api

---
+++ Sorux.Framework.Bot.Core.Interface/PluginsSDK/SDK/FutureTest/QQ/Models/ImageOcrModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Sorux.Framework.Bot.Core.Interface.PluginsSDK.SDK.FutureTest.QQ.Models;
+
+public class ImageOcrModel
+{
+    public List<TextDetection> texts { get; set; }
+    /// <summary>
+    /// 语言
+    /// </summary>
+    public string language { get; set; }
+}
